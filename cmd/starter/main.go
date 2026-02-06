c533fb6f0553438823273d3c1a57329ca183e431
bananapie228 <galimzhan19358@gmail.com>
2026-02-06 16:05:34 +0500

    git commit -m "feat: complete end-to-end integration of Temporal worker with Postgres - added cmd/worker for service execution - added cmd/starter for integration testing"

--- cmd/starter/main.go
+++ cmd/starter/main.go
@@ -2,32 +2,44 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 
-	"go.temporal.io/sdk/client"
+	"async-file-storage/internal/repository"
+	"async-file-storage/internal/temporal"
 
-	t "async-file-storage/internal/temporal"
+	"go.temporal.io/sdk/client"
 )
 
 func main() {
+	dsn := "postgres://galym:galik2006@localhost:5433/downloader?sslmode=disable"
+	repo, _ := repository.NewPostgresRepository(dsn)
+
+	urls := []string{
+		"https://raw.githubusercontent.com/temporalio/samples-go/master/README.md",
+		"https://google.com",
+	}
+
+	requestID, err := repo.CreateRequest(context.Background(), urls)
+	if err != nil {
+		log.Fatalf("Failed to create request in DB: %v", err)
+	}
+
 	c, err := client.Dial(client.Options{})
 	if err != nil {
-		log.Fatalln("connection error", err)
+		log.Fatalln("Unable to create Temporal client", err)
 	}
 	defer c.Close()
 
-	// 2. Запускаем Workflow
-	workflowOptions := client.StartWorkflowOptions{
-		ID:        "test-workflow-id",
-		TaskQueue: "test-queue",
+	// 4. Запускаем Workflow
+	options := client.StartWorkflowOptions{
+		ID:        "download_request_test",
+		TaskQueue: "file-storage-tasks",
 	}
 
-	we, err := c.ExecuteWorkflow(context.Background(), workflowOptions, t.SayHelloWorkflow, "gala")
+	we, err := c.ExecuteWorkflow(context.Background(), options, temporal.DownloadWorkflow, requestID, urls)
 	if err != nil {
-		log.Fatalln("could not load workflow", err)
+		log.Fatalln("Unable to execute workflow", err)
 	}
 
-	fmt.Printf("\n Workflow running! ID: %s, RunID: %s\n", we.GetID(), we.GetRunID())
-
+	log.Printf("Workflow started! RequestID: %d, RunID: %s\n", requestID, we.GetRunID())
 }
--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -3,27 +3,36 @@ package main
 import (
 	"log"
 
+	"async-file-storage/internal/repository"
+	"async-file-storage/internal/temporal"
+
 	"go.temporal.io/sdk/client"
 	"go.temporal.io/sdk/worker"
-
-	t "async-file-storage/internal/temporal"
 )
 
 func main() {
+	dsn := "postgres://galym:galik2006@localhost:5433/downloader?sslmode=disable"
+	repo, err := repository.NewPostgresRepository(dsn)
+	if err != nil {
+		log.Fatalf("Failed to init repository: %v", err)
+	}
+
 	c, err := client.Dial(client.Options{})
 	if err != nil {
-		log.Fatalln("could not connect Temporal Client", err)
+		log.Fatalf("Failed to create Temporal client: %v", err)
 	}
 	defer c.Close()
 
-	w := worker.New(c, "test-queue", worker.Options{})
+	w := worker.New(c, "file-storage-tasks", worker.Options{})
 
-	w.RegisterWorkflow(t.SayHelloWorkflow)
-	w.RegisterActivity(t.Greet)
+	w.RegisterWorkflow(temporal.DownloadWorkflow)
 
-	// 4. Запускаем
+	activityContainer := &temporal.Activities{Repo: repo}
+	w.RegisterActivity(activityContainer)
+
+	log.Println("Worker is starting...")
 	err = w.Run(worker.InterruptCh())
 	if err != nil {
-		log.Fatalln("worker failed", err)
+		log.Fatalf("Worker failed: %v", err)
 	}
 }
