a48ec09b0c8136ada2d6b1b7b0b3842c7ee6b8f5
bananapie228 <galimzhan19358@gmail.com>
2026-02-11 19:40:43 +0500

    fix(domain): restore models

--- internal/domain/models.go
+++ internal/domain/models.go
@@ -1,1 +1,25 @@
 package domain
+
+import "time"
+
+type Status string
+
+const (
+	StatusProcess Status = "PROCESS"
+	StatusDone    Status = "DONE"
+	StatusError   Status = "ERROR"
+)
+
+type DownloadRequest struct {
+	ID        int
+	Status    Status
+	CreatedAt time.Time
+}
+
+type FileEntry struct {
+	ID        int
+	RequestID int
+	URL       string
+	Data      []byte
+	Error     string
+}
